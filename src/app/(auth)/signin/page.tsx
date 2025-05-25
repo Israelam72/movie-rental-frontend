b37f80094528fb55328da44353a930cfa7d03bf3
Israelam72 <israelarantes.m@gmail.com>
2025-05-25 17:33:51 -0300

    feat: implement SignIn form with validation and UI components
    - Added SignIn form using react-hook-form and zod for validation.
    - Created schema for email and password validation.
    - Integrated new UI components for form handling, including Form, FormField, and Input.
    - Updated package.json and yarn.lock to include necessary dependencies.

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,6 +1,24 @@
+"use client"
+
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { type SignInData, signInSchema } from "./schema"
+import { Button } from "@/components/ui/button"
 import { SignInBackgroundDecorators, SignInDecorators, CardDetails, TextDetails } from "@/components/decorators/signInDecorators"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
 
 const SignIn = () => {
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema)
+  })
+
+  const onSubmit = (data: z.infer<typeof signInSchema>) => {
+    console.log(data)
+  }
+
   return (
     <div className="flex flex-col bg-background items-center justify-center h-screen">
       <SignInBackgroundDecorators />
@@ -15,6 +33,37 @@ const SignIn = () => {
                 <TextDetails />
                 <p className="text-gray-400">Welcome back to RENTAMOVIE</p>
               </div>
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                  <FormField 
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel htmlFor="email">Email</FormLabel>
+                        <FormControl>
+                          <Input id="email" type="email" placeholder="Enter your email" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField 
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel htmlFor="password">Password</FormLabel>
+                        <FormControl>
+                          <Input id="password" type="password" placeholder="Enter your password" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit" className="w-full bg-pink-md text-background rounded-none hover:bg-pink-dark transform -skew-x-6 transition-colors duration-300 hover:translate-x-[-1px] hover:translate-y-[1px] active:translate-x-[-4px] active:translate-y-[4px] transition-transform duration-100">Sign In</Button>
+                </form>
+              </Form>
             </div>
           </div>
         </div>
